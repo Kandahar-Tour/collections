64083c9c4dd4b933defd96ef0d7a5fdc545fd01d
mansoor faizi <mansoorfaizi.cs05@gmail.com>
2022-11-01 21:16:29 +0430

    List collection added

---
+++ collections/List.py
@@ -0,0 +1,27 @@
+li = [1, 2, 3, 4, 5]
+for i in li:
+    print(i)
+
+li.append(6)
+print(li)
+
+li.insert(0, 0)
+print(li)
+
+li.remove(0)
+print(li)
+
+li.pop()
+print(li)
+
+li.sort()
+print(li)
+
+li.reverse()
+print(li)
+
+li.copy()
+print(li)
+
+li.clear()
+print(li)
