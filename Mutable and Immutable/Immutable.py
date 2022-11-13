1aeec704c540553cdb5632622b5bc30d70acd24e
mansoor faizi <mansoorfaizi.cs05@gmail.com>
2022-11-13 21:41:33 +0430

    mutable and immutable added

---
+++ Mutable and Immutable/Immutable.py
@@ -0,0 +1,14 @@
+# immutable objects
+
+
+tuple1 = (0,1,2,3)
+
+tuple1[0] = 4
+
+print(tuple1)
+
+
+message = "welcome to the first class"
+
+message[0] =  'p'
+print(message)
---
+++ Mutable and Immutable/Mutable.py
@@ -0,0 +1,9 @@
+# mutable objects
+
+
+color = ['red', 'blue', 'green']
+print(color)
+
+color[0] = 'pink'
+color[-1] = 'orange'
+print(color)
