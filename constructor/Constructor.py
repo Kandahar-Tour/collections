a1151460953cafc06d36e19217e0d4762a0672fa
mansoor faizi <mansoorfaizi.cs05@gmail.com>
2022-11-13 19:06:11 +0430

    constructor added

---
+++ constructor/Constructor.py
@@ -0,0 +1,11 @@
+class Constructor:
+    # default Constructor 
+    def __init__(self):
+        self.name = "Mansoor"
+    
+    def __str__(self):
+        return self.name
+
+t = Constructor()
+print(t)
+
