915707904275ab493c18afe5ddf472246fad9157
mansoor faizi <mansoorfaizi.cs05@gmail.com>
2022-11-15 07:27:44 +0430

    modified scop file

--- scop/Scop.py
+++ scop/Scop.py
@@ -1,7 +1,25 @@
 def myFunc():
+    # local variable 
     name = "Mohammad"
     def myInnerFunc():
         print(name)
     myInnerFunc()
 
 myFunc()
+
+
+# global variable 
+age = 21
+
+def myAge():
+    print("My age is :", age)
+
+myAge()
+
+
+class Show:
+    def show(self):
+        print(age)
+
+t = Show()
+print(t.show())
